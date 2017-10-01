29573c5f0cf0c1fb9e26d530f83d80520023cad9
GalenSheen <>
2017-10-02 00:42:54 +0800

    init project

---
+++ src/child.jsx
@@ -0,0 +1,9 @@
+/**
+ * Created by GalenSheen on 01/10/2017.
+ */
+
+export default function Child() {
+    return (
+        <div>child component</div>
+    );
+}
---
+++ src/index.js
@@ -0,0 +1,7 @@
+/**
+ * Created by GalenSheen on 01/10/2017.
+ */
+
+import Child from './child.jsx';
+
+export {Child};
